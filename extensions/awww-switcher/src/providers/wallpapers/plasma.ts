2fb319ed402eaa3f9408a31f99f002df68502667
Lyna <40660493+0x4c756e61@users.noreply.github.com>
2025-12-10 16:40:29 +0100

    feat: Add plasma provider

---
+++ extensions/awww-switcher/src/providers/wallpapers/plasma.ts
@@ -0,0 +1,60 @@
+import { WindowManagement } from "@vicinae/api/dist";
+import { WallpaperProvider } from "../../models/wallpaper-provider";
+import { sessionBus, MessageBus, Variant } from "dbus-next";
+
+interface PlasmaShellInterface {
+  setWallpaper(
+    pluginId: string,
+    pluginOptions: {
+      Image: Variant;
+    },
+    screenIndex: number,
+  ): Promise<void>;
+}
+
+export class PlasmaWallpaper implements WallpaperProvider {
+  bus: MessageBus;
+
+  constructor() {
+    this.bus = sessionBus();
+  }
+
+  serverIsRunning(): boolean {
+    throw new Error("Method not implemented.");
+  }
+
+  async setWallpaper(
+    path: string,
+
+    //We do not yet support specifying the screen on kde plasma, as such this option will never be used
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _monitor?: WindowManagement.Screen,
+  ): Promise<void> {
+    try {
+      const proxy = await this.bus.getProxyObject(
+        "org.kde.plasmashell",
+        "/PlasmaShell",
+      );
+
+      const shell = proxy.getInterface(
+        "org.kde.PlasmaShell",
+      ) as unknown as PlasmaShellInterface;
+
+      await shell.setWallpaper(
+        "org.kde.image",
+        {
+          Image: new Variant("s", path),
+        },
+        0,
+      );
+
+      return Promise.resolve();
+    } catch (error) {
+      if (error instanceof Error) {
+        return Promise.reject(error.message);
+      }
+
+      return Promise.reject("An unknown error occured");
+    }
+  }
+}
